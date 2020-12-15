0cd8a741961287b3424aff5a7a6f664429675d72
yudis <yudistiradoremi@gmail.com>
2020-12-15 23:42:39 +0700

    add mapState, mapMutations, and update some methods

--- pages/form/Form.js
+++ pages/form/Form.js
@@ -1,45 +1,74 @@
+import {mapState, mapMutations} from 'vuex';
+
 export default {
-    head() {
-        return {
-            title: "Calculator Page"
-        };
-    },
-    state: {
-        errors: []
-    },
-    data() {
-        const saldo = 15;
-        var calcForms = [
-            {price : '', action : '', ammount: '', errors: ''},
-            {price : '', action : '', ammount: '', errors: ''}
-        ]
-        return {
-            saldo: saldo,
-            calcForms: calcForms,
+    created() {
+        if(localStorage.getItem('calcForm')){
+            this.updateForm(JSON.parse(localStorage.getItem('calcForm')))
         }
     },
-    computed: {
-        getErrors(){
-            return this.errors
-        }
+    computed : {
+        ...mapState({
+            calcForms: state => state.calcForms,
+            saldo: state => state.saldo,
+        })
     },
     methods : {
-        addRow(){
+        ...mapMutations({
+            addFormRow: "addFormRow",
+            removeFormRow: "removeFormRow",
+            onChangePrice: "onChangePrice",
+            onChangeAmmount: "onChangeAmmount",
+            onchangeAction: "onchangeAction",
+            updateForm: "updateForm",
+            onChangeSaldo: "onChangeSaldo"
+
+        }),
+        addNewRow(){
             if(this.calcForms.length < this.saldo){
-                 this.calcForms.push(
-                    {price : '', action : '', ammount: ''}
-                )
+                this.addFormRow()
             }
         },
         removeRow(index){
-            console.log('removeRow', index)
-            if(this.calcForms.length > 1) {
-                this.calcForms.splice(index, 1)
+            this.removeFormRow(index)
+        },
+        changePrice(event, index){
+            const payload = {
+                value: event.target.value,
+                index: index
             }
+            this.onChangePrice(payload)
+        },
+        changeAmmount(event, index){
+            const payload = {
+                value: event.target.value,
+                index: index
+            }
+            this.onChangeAmmount(payload)
+        },
+        changeSaldo(event){
+            this.onChangeSaldo(event.target.value)
         },
         doCalc(){
             localStorage.setItem('calcForm', JSON.stringify(this.calcForms))
             this.$router.replace('/summary');
+        },
+        changeAction(event, index) {
+            var result = this.calcForms.find(obj => {
+                return obj.action === "buy"
+            })
+            if(result != undefined){
+                const payload = {
+                    value: event.target.value,
+                    index: index
+                }
+                this.onchangeAction(payload)
+            }
+            
         }
     },
+    head() {
+        return {
+            title: "Calculator Page"
+        };
+    }
 }
